a54d1d9bf686117f14e7a23c346f06acfe759da3
Ruben <churrerico@web.de>
2021-12-31 13:59:56 +0100

    Improved Youtube Search

--- music.py
+++ music.py
@@ -1,5 +1,7 @@
-import discord, youtube_dl
+import discord, youtube_dl, youtube_search, validators
 from discord.ext import commands
+
+color = 0xAAA9AD
 
 class music(commands.Cog):
     def __init__(self, client):
@@ -25,7 +27,7 @@ class music(commands.Cog):
         await ctx.voice_client.disconnect()
 
     @commands.command()
-    async def play(self, ctx, url):
+    async def play(self, ctx, *url):
         if ctx.author.voice is None:
             await ctx.channel.send("Not in a voice Channel")
             return
@@ -39,6 +41,25 @@ class music(commands.Cog):
         FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", "options": "-vn"}
         YDL_OPTIONS = {"format": "bestaudio"}
         vc = ctx.voice_client
+        url = " ".join(url)
+
+        if not validators.url(url):
+            video = youtube_search.YoutubeSearch(url, max_results=1).to_dict()[0]
+            url = f"https://www.youtube.com/watch?v={video['id']}"
+            videoData = {"views": video["views"], "duration": video["duration"], "thumbnail": video["thumbnails"][0]}
+        else:
+            video = youtube_search.YoutubeSearch(url, max_results=1).to_dict()[0]
+            videoData = {"title": video["title"], "views": video["views"], "duration": video["duration"], "thumbnail": video["thumbnails"][0]}
+        
+        embed = discord.Embed(title=videoData["title"], color=color)
+
+        for key in videoData.keys():
+            if key == "thumbnail":
+                embed.set_thumbnail(url=videoData[key])
+            else:
+                embed.add_field(name=key.capitalize(), value=videoData[key])
+
+        await ctx.channel.send(embed)
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
